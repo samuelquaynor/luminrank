280565ad300f575f7dbe17c6bf47d06195767d1b
Samuel Williams <samwillsquaye@gmail.com>
2025-10-08 19:46:38 +0000

    fix: set Server render mode for dynamic league join route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'leagues/join/:code',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
